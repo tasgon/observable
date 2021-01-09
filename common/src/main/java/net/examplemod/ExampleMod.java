cc6d68ae3ad451eae125c0877226b14980ac6395
shedaniel <daniel@shedaniel.me>
2021-01-09 22:37:24 +0800

    Update example mod

--- common/src/main/java/net/examplemod/ExampleMod.java
+++ common/src/main/java/net/examplemod/ExampleMod.java
@@ -1,24 +1,35 @@
 package net.examplemod;
 
 import me.shedaniel.architectury.registry.CreativeTabs;
+import me.shedaniel.architectury.registry.DeferredRegister;
 import me.shedaniel.architectury.registry.Registries;
+import me.shedaniel.architectury.registry.RegistrySupplier;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.LazyLoadedValue;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+
+import java.util.function.Supplier;
 
 public class ExampleMod {
     public static final String MOD_ID = "examplemod";
+    // We can use this if we don't want to use DeferredRegister
     public static final LazyLoadedValue<Registries> REGISTRIES = new LazyLoadedValue<>(() -> Registries.get(MOD_ID));
     // Registering a new creative tab
-    public static final CreativeModeTab EXAMPLE_TAB = CreativeTabs.create(new ResourceLocation(MOD_ID, "example_tab"), () -> new ItemStack(Items.DIAMOND_SWORD));
+    public static final CreativeModeTab EXAMPLE_TAB = CreativeTabs.create(new ResourceLocation(MOD_ID, "example_tab"), new Supplier<ItemStack>() {
+        @Override
+        public ItemStack get() {
+            return new ItemStack(EXAMPLE_ITEM.get());
+        }
+    });
+    
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registry.ITEM_REGISTRY);
+    public static final RegistrySupplier<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () ->
+            new Item(new Item.Properties().tab(ExampleMod.EXAMPLE_TAB)));
     
     public static void init() {
-        // Using the architectury registry to defer the creation of new entries to when they are registered.
-        REGISTRIES.get().get(Registry.ITEM_REGISTRY).register(new ResourceLocation(MOD_ID, "example_item"), () ->
-                new Item(new Item.Properties().tab(ExampleMod.EXAMPLE_TAB)));
+        ITEMS.register();
     }
 }
