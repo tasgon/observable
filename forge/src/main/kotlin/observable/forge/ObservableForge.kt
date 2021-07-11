e7dbc99be35355a1d95159eadc81b19a87cd85cd
tasgon <tasgon_@outlook.com>
2021-07-11 03:46:25 -0400

    moving to coremods

--- forge/src/main/kotlin/observable/forge/ObservableForge.kt
+++ forge/src/main/kotlin/observable/forge/ObservableForge.kt
@@ -2,7 +2,9 @@ package observable.forge
 
 import observable.Observable.init
 import me.shedaniel.architectury.platform.forge.EventBuses
+import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import observable.Observable
 
@@ -11,6 +13,12 @@ class ObservableForge {
     init {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(Observable.MOD_ID, FMLJavaModLoadingContext.get().modEventBus)
+        MinecraftForge.EVENT_BUS.register(EventTester)
+        FMLJavaModLoadingContext.get().modEventBus.addListener(this::onClientInit)
         init()
+    }
+
+    fun onClientInit(ev: FMLClientSetupEvent) {
+        Observable.clientInit()
     }
 }
