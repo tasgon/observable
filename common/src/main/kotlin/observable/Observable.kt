d6cd78fc0f878ce1cb09730e9ef833f379726b43
tasgon <tasgon_@outlook.com>
2022-11-07 03:34:54 -0800

    actually add files

--- common/src/main/kotlin/observable/Observable.kt
+++ common/src/main/kotlin/observable/Observable.kt
@@ -28,7 +28,6 @@ import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.level.Level
 import net.minecraft.world.phys.Vec3
 import observable.client.Overlay
 import observable.client.ProfileScreen
