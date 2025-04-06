f14586bc414986e9eff2acb9f67a0cd4cab44b6b
tasgon <me@tas.sh>
2025-04-06 01:21:55 -0700

    cleanup imports

--- common/src/main/java/observable/mixin/ServerLevelMixin.java
+++ common/src/main/java/observable/mixin/ServerLevelMixin.java
@@ -4,14 +4,12 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
 import observable.Observable;
 import observable.Props;
 import observable.server.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
